ae6a55248ba4110952b95bfc1e71e866560c089b
deanc4 <deanc4@wwu.edu>
2018-10-25 21:37:55 -0700

    fresh start

---
+++ app/src/test/java/lilpupper/com/groc/MainActivityUnitTest.java
@@ -0,0 +1,12 @@
+package lilpupper.com.groc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityUnitTest {
+    @Test
+    public void onLoad_displaysGroceryList() throws Exception {
+//        assertEquals(4, 2 + 2);
+    }
+}
